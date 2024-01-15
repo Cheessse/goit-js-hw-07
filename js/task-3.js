8ae7c88b05c7b94105538d91c0cf606e5a23444e
Cheessse <davidprekaska12345@gmail.com>
2024-01-15 18:54:40 +0200

    completed homework

---
+++ js/task-3.js
@@ -0,0 +1,13 @@
+const inputName = document.querySelector("#name-input");
+const outputName = document.querySelector("#name-output");
+
+function inputValue() {
+  const inputValue = inputName.value.trim();
+  if (inputValue === "") {
+    outputName.textContent = "Anonymous";
+  } else {
+    outputName.textContent = inputValue;
+  }
+}
+
+inputName.addEventListener("input", inputValue);
